46224f1a506fdbbe5192b26aae7be51bd6518970
mironov-anton <miron.khuzd@gmail.com>
2022-01-16 20:20:56 +0300

    added namespaces; made CBVs for movies, directors, genres; added GET methods for CBVs

--- app.py
+++ app.py
@@ -68,5 +68,61 @@ directors_schema = DirectorSchema(many=True)
 genre_schema = GenreSchema()
 genres_schema = GenreSchema(many=True)
 
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        all_movies = Movie.query.all()
+        return movies_schema.dump(all_movies), 200
+
+
+@movie_ns.route('/<int:mid>')
+class MovieView(Resource):
+    def get(self, mid: int):
+        try:
+            movie = db.session.query(Movie).filter(Movie.id == mid).one()
+            return movie_schema.dump(movie), 200
+        except Exception:
+            return "", 404
+
+
+@director_ns.route('/')
+class DirectorsView(Resource):
+    def get(self):
+        all_directors = Director.query.all()
+        return directors_schema.dump(all_directors), 200
+
+
+@director_ns.route('/<int:did>')
+class DirectorView(Resource):
+    def get(self, did: int):
+        try:
+            director = db.session.query(Director).filter(Director.id == did).one()
+            return director_schema.dump(director), 200
+        except Exception:
+            return "", 404
+
+
+@genre_ns.route('/')
+class GenresView(Resource):
+    def get(self):
+        all_genres = Genre.query.all()
+        return genres_schema.dump(all_genres), 200
+
+
+@genre_ns.route('/<int:gid>')
+class GenreView(Resource):
+    def get(self, gid: int):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == gid).one()
+            return genre_schema.dump(genre), 200
+        except Exception:
+            return "", 404
+
 if __name__ == '__main__':
     app.run(debug=True)
